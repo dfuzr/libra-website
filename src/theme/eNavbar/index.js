8963f629419698a24be4aff8253020f38450151b
oriooctopus <oliverullman@gmail.com>
2020-06-03 22:17:30 -0600

    build investigation

---
+++ src/theme/eNavbar/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import {Nav} from 'libra-docusaurus';
+
+const Navbar = () => {
+  return null;
+  // return <Nav siteTitle="Libra Docs" siteID="developers" />
+};
+
+export default Navbar;
